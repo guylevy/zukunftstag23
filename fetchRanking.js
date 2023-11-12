5992ff3642f4cda81a867e9a46e78cd62ee1b4c9
kobedingdong <150165271+kobedingdong@users.noreply.github.com>
2023-11-12 20:09:33 +0100

    Create fetchRanking.js

---
+++ fetchRanking.js
@@ -0,0 +1,33 @@
+// Use the Fetch API to fetch the webpage content
+fetch('https://sfl.ch/de/cssl_ranking')
+  .then(response => {
+    if (response.ok) {
+      return response.text();
+    } else {
+      throw new Error(`Failed to fetch data. Status code: ${response.status}`);
+    }
+  })
+  .then(html => {
+    // Create a DOM parser to parse the HTML content
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
+
+    // Locate the table element on the page (you may need to inspect the page's structure)
+    const table = doc.querySelector('.your-table-class'); // Replace with the actual class or selector of the table
+
+    if (table) {
+      // Access and manipulate the table data as needed
+      const rows = table.querySelectorAll('tr');
+      rows.forEach(row => {
+        const columns = row.querySelectorAll('td');
+        columns.forEach(column => {
+          console.log(column.textContent);
+        });
+      });
+    } else {
+      console.log('Table not found on the page.');
+    }
+  })
+  .catch(error => {
+    console.error(error);
+  });
